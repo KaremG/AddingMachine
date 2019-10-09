74ebeef2a540cb2bc75d6cc844ef861e41f122d2
Karem Gonzalez <kar2171233@maricopa.edu>
2019-10-09 10:31:44 -0700

    Initial check-in.

---
+++ AddingMachine/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,61 @@
+package cse360assign2;
+
+/**
+ * Assignment 2
+ * Class ID: 70641
+ * @author	Karem Gonzalez
+ * @version	October 9, 2019
+*/
+
+ /**
+  * The class AddingMachine will add, subtract, and keep track of the total.
+ */
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Constructor	
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Add method will add value to total
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtract method will subtract value from total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * toString method keeps history of the transactions	
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Clear method will erase the history.
+	 */
+	public void clear() {
+	
+	}
+}
